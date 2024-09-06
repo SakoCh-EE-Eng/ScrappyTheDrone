b213efe0f513c097766275035d6bb025a77b403c
SakoCh-EE-Eng <sarkischekerjian@gmail.com>
2024-09-05 20:03:59 -0700

    Methods added to header (#2)
    Co-authored-by: Sako <67485253+sakorocco@users.noreply.github.com>

--- source/main.hpp
+++ source/main.hpp
@@ -1,4 +1,14 @@
+#include <string>
+class main{
+
+    
+void run() {};
 
 int calculate_duty_cycle(float throttle, float dead_zone) {};
 
 int translate_pair(int high, int low) {};
+
+float normalize(float value,float original_min, float original_max, float new_min, float new_max){};
+
+void FATA_ERROR(std::string msg){};
+};
