1dae4713dbc6128eda7c1f3052e49d1d5a3ec53b
Sako <67485253+sakorocco@users.noreply.github.com>
2024-09-05 19:50:06 -0700

    Methods added to header

---
+++ source/main.hpp
@@ -0,0 +1,10 @@
+#include <string>
+class main{
+
+    
+void run() {};
+
+float normalize(float value,float original_min, float original_max, float new_min, float new_max){};
+
+void FATA_ERROR(std::string msg){};
+};
